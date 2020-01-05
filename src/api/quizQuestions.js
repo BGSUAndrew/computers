959128f8dcf27147f6c7db7ead22ffe90ed96fd2
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2020-01-05 16:27:15 -0500

    A quiz I'm builiding
    Quiz built to help casual computer users decide on a new machine.

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,34 @@
+
+var quizQuestions = [
+  {
+      question: "Which operating system do you prefer?",
+      answers: [
+          {
+              computer: "Apple Macbook",
+              answer: "OS X"
+          },
+          {
+              computer: "Dell Inspron",
+              answer: "Windows"
+          },
+      ]
+  },
+  {
+    question: "What is your price range?",
+    answers: [
+        {
+            computer: "Apple Macbook",
+            answer: "$1,000 and up",
+        },
+        {
+            computer: "Dell Inspron",
+            answer: "$300-500"
+        },
+    ]
+},
+];
+
+export default quizQuestions;
+
+  
+
